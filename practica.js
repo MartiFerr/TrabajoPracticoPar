c0edde991262d0b197b92e82b4da45d73c826a8b
alumno <alumno@Instituto.local>
2023-10-20 09:38:28 -0300

    cambio

---
+++ practica.js
@@ -0,0 +1,44 @@
+/* 1 1. Crear una funcion que determiene el mayor de 2 umeros
+2. Crearuna funcion que determine si un umero es positivo, egativo o cero
+3. Crear ua funcion que determine si u numero esta entre los valores 25 y 255
+*/
+
+
+function numeromayor (a,b){
+    if (a > b)
+    {
+        return a
+    }
+    else{
+        return b
+    }
+}
+
+function positivonagativocero (num){
+    if (num > 0) 
+{
+    return "positivo"
+} else if (num == 0){
+    return "cero"
+} else {
+    return"negativo"
+}
+}
+
+
+funcion 
+ if 
+     ((num> 25) && ( num<255))
+  {
+return "esta e el rango de valor"
+}
+else{
+    return "no esta en el rago de valores"
+}
+
+
+
+
+
+
+
